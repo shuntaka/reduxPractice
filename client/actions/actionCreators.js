949de9a5af3c567609be8276088ad0123637ae99
shuntaka <chang.shoon@gmail.com>
2017-02-22 23:57:19 +0900

    upto 16

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,28 @@
+// increment
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LINKS',
+    index
+  }
+}
+
+// add comment
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+
+// remove comment
+export function removeComment(postId, i) {
+  console.log('remove comment');
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
